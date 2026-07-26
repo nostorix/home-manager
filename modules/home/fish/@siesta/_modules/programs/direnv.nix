0099a3736cc5ecc8cc800d768354737ef3b88a17
fish <kiokunee@gmail.com>
2026-07-26 17:30:48 +0800

    fish@siesta: add direnv

---
+++ modules/home/fish/@siesta/_modules/programs/direnv.nix
@@ -0,0 +1,16 @@
+{ ... }: {
+  nixpkgs.overlays = [
+    (self: super: {
+      nix-direnv = super.nix-direnv.override {
+        nix = super.nix-monitored;
+      };
+    })
+  ];
+  programs = {
+    direnv = {
+      enable = true;
+      enableZshIntegration = true;
+      nix-direnv.enable = true;
+    };
+  };
+}
